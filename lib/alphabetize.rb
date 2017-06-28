41a01c98e343c710c3424aed414e91e4fd7ecc03
gabidavila <gabidavila@gmail.com>
2017-06-28 18:47:12 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,7 @@
-def alphabetize(arr)
-  # code here
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+ASCII = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
+def alphabetize(words)
+  words.sort_by do |word|
+    word.tr(ESPERANTO_ALPHABET, ASCII)
+  end
 end
